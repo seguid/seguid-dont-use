5797566df7d9db4c98ebd7121e06364271aa7cc6
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-10-14 12:07:07 -0700

    R: Add working seguid()

--- r/R/seguid.R
+++ r/R/seguid.R
@@ -7,14 +7,24 @@
 #'
 #' @examples
 #' seguid("ACGTACGTACGT")
+#' seguid("acgtACGTacgt")
+#' seguid("ACGTACGTACGT") == seguid("acgtACGTacgt")
 #'
 #' @references
 #' 1. Babnigg G, Giometti CS. A database of unique protein sequence
 #'    identifiers for proteome studies. Proteomics.
 #'    2006 Aug;6(16):4514-22. \doi{10.1002/pmic.200600032}.
 #'
+#' @importFrom base64enc base64encode
+#' @importFrom digest digest
 #' @export
 seguid <- function(seq) {
+  seq <- toupper(seq)
+  checksum <- digest::digest(seq, algo = "sha1", serialize = FALSE, raw = TRUE)
+  checksum <- base64encode(checksum)
+  checksum <- sub("[\n]+$", "", checksum)
+  checksum <- sub("[=]$", "", checksum)
+  checksum
 }
 
 
