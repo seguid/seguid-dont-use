323f2df77961fc59129dffaee65f0d3cd3aec3c8
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-10-14 13:22:47 -0700

    R: Add one more test for useguid()

--- r/tests/useguid.R
+++ r/tests/useguid.R
@@ -5,3 +5,6 @@ stopifnot(
   useguid("aAA") == "YG7G6b2Kj_KtFOX63j8mRHHoIlE"
 )
 
+x <- "tcgcgcgtttcggtgatgacggtgAAAAcctctgacacatgcagctcccggattgtactgagagtgc"
+stopifnot(useguid(x) == "cl5ukSUdlvZeBaBLEUhxisdRaL8")
+
