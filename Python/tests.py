55f455f41133d7b2e25b7b8714c5b278b55ad90c
BjornFJohansson <bjornjobb@gmail.com>
2023-10-19 09:12:05 +0100

    tests for lseguid_blunt

--- Python/tests.py
+++ Python/tests.py
@@ -2,7 +2,7 @@ x = "tcgcgcgtttcggtgatgacggtgAAAAcctctgacacatgcagctcccggattgtactgagagtgc"
 
 
 def test_lseguid_blunt():
-    from seguids import lseguid_blunt
+    from seguid import lseguid_blunt
     assert (
         lseguid_blunt(x)
         == lseguid_blunt(x.upper())
@@ -11,18 +11,50 @@ def test_lseguid_blunt():
     )
 
 def test_lseguid_sticky():
-    from seguids import lseguid_sticky
+    from seguid import lseguid_sticky
+    from seguid import tuple_from_representation
+    from seguid import useguid
+
+    case1 = tuple_from_representation("""
+       TATGCC
+      catacg
+
+    """)
+    assert case1 == ('TATGCC', 'gcatac', 1)
+
+    assert lseguid_sticky(*case1) == 'Jv9Z9JJ0IYnG-dTPBGwhDyAqnmU'
+    assert lseguid_sticky(*case1) == useguid(" gcatac\nCCGTAT")
+
+    case2 = tuple_from_representation("""
+      gTATGC
+      catacg
+
+    """)
+    assert case2 == ('gTATGC', 'gcatac', 0)
+    assert lseguid_sticky(*case2) == 'RAgd7GiTGrnLcI2VQ55u-lZiGsw'
+    assert lseguid_sticky(*case2) == useguid("gcatac") # Here dsSEGUID is different!
+
+    case3 = tuple_from_representation("""
+      gTATGC
+       atacgc
+
+    """)
+    assert case3 == ('gTATGC', 'cgcata', -1)
+
+    assert lseguid_sticky(*case3) == '3PZNLU2tPDYs78AJP3mg5w1uEw4'
+
+    assert lseguid_sticky(*case3) == useguid("cgcata\n CGTATg")
 
 
 def test_useguid():
-    from seguids import useguid
+    from seguid import useguid
     assert useguid(x) == "cl5ukSUdlvZeBaBLEUhxisdRaL8"
     assert useguid(x.upper()) == "cl5ukSUdlvZeBaBLEUhxisdRaL8"
     assert useguid(x.lower()) == "cl5ukSUdlvZeBaBLEUhxisdRaL8"
 
 
 def test_cseguid():
-    from seguids import cseguid
+    from seguid import cseguid
     assert cseguid(x) == "naaZmDzyMa58OsNXROe5SvjC7WU"
     assert cseguid(x) == cseguid(x.upper())
     assert cseguid(x) == cseguid(x.lower())
@@ -30,7 +62,7 @@ def test_cseguid():
 
 def test_tuple_from_representation():
 
-    from seguids import tuple_from_representation
+    from seguid import tuple_from_representation
 
     rprs = {}
 
