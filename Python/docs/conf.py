f037f10dbf8fcb10d90d65f856e50ef9523b325a
BjornFJohansson <bjornjobb@gmail.com>
2023-10-20 09:08:34 +0100

    try to fix rtd

--- Python/docs/conf.py
+++ Python/docs/conf.py
@@ -16,7 +16,7 @@ release = '0.0.1'
 
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
-              'sphinx.ext.napoleon'
+              'sphinx.ext.napoleon',
               "sphinx.ext.doctest",
               "sphinx.ext.viewcode",
               "sphinx.ext.autosummary",
