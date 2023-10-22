48b79d2702b2a37a81bcbb480c45a65bf98c51ad
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-10-21 19:31:21 -0700

    Add more tests for tuple_from_representation()

--- r/tests/tuple_from_representation.R
+++ r/tests/tuple_from_representation.R
@@ -2,6 +2,11 @@ library(seguid)
 
 tuple_from_representation <- seguid:::tuple_from_representation
 
+# ----------------------------------------------------------------
+# Test 1
+# ----------------------------------------------------------------
+truth <- list(watson = "TATGCC", crick = "gcatac", overhang = 1L)
+
 rprs <- list()
 
 rprs[[1]] <- "   \
@@ -32,7 +37,51 @@ rprs[[4]] <- "   \
                  \
 "
 
-truth <- list(watson = "TATGCC", crick = "gcatac", overhang = 1L)
+tuples <- lapply(rprs, FUN = function(rpr) {
+  cat(rpr, sep = "")
+  tuple <- tuple_from_representation(rpr)
+  str(tuple)
+  stopifnot(identical(tuple, truth))
+})
+
+
+
+
+# ----------------------------------------------------------------
+# Test 2
+# ----------------------------------------------------------------
+cat("--------------------------------\n")
+truth <- list(watson = "TATGCC", crick = "ggcata", overhang = 0L)
+
+rprs <- list()
+
+rprs[[1]] <- "   \
+                 \
+    5'-TATGCC-3' \
+       ||||||    \
+    3'-atacgg-5' \
+                 \
+"
+
+rprs[[2]] <- "   \
+                 \
+    5'-TATGCC-3' \
+       ||||||    \
+    3'-atacgg-5' \
+                 \
+"
+
+rprs[[3]] <- "   \
+       TATGCC    \
+       ||||||    \
+       atacgg    \
+"
+
+rprs[[4]] <- "   \
+   TATGCC        \
+   atacgg        \
+                 \
+"
 
 tuples <- lapply(rprs, FUN = function(rpr) {
   cat(rpr, sep = "")
@@ -42,3 +91,46 @@ tuples <- lapply(rprs, FUN = function(rpr) {
 })
 
 
+
+# ----------------------------------------------------------------
+# Test 3
+# ----------------------------------------------------------------
+cat("--------------------------------\n")
+truth <- list(watson = "TATGCC", crick = "ggggcat", overhang = -1L)
+
+rprs <- list()
+
+rprs[[1]] <- "   \
+                 \
+  5'-TATGCC-3'   \
+      |||||      \
+   3'-acgggg-5'  \
+                 \
+"
+
+rprs[[2]] <- "   \
+                 \
+  5'-TATGCC-3'   \
+      |||||      \
+   3'-acgggg-5'  \
+                 \
+"
+
+rprs[[3]] <- "   \
+     TATGCC      \
+      |||||      \
+      acgggg     \
+"
+
+rprs[[4]] <- "   \
+     TATGCC      \
+      acgggg     \
+                 \
+"
+
+tuples <- lapply(rprs, FUN = function(rpr) {
+  cat(rpr, sep = "")
+  tuple <- tuple_from_representation(rpr)
+  str(tuple)
+#  stopifnot(identical(tuple, truth))
+})
