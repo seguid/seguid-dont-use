f675eece44fa1d6d8644f541ae02ca4142b67338
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-10-21 18:23:36 -0700

    Update tests

--- r/tests/lseguid_blunt.R
+++ r/tests/lseguid_blunt.R
@@ -1,4 +1,18 @@
 library(seguid)
 
+reverse_sequence <- seguid:::reverse_sequence
+rc <- seguid:::rc
+space <- seguid:::space
+
 x <- "tcgcgcgtttcggtgatgacggtgAAAAcctctgacacatgcagctcccggattgtactgagagtgc"
-stopifnot(lseguid_blunt(x) == "bHrqalTJ793oAigMQ5_qCttJRTk")
+truth <- "bHrqalTJ793oAigMQ5_qCttJRTk"
+##truth <- "R7VPKJXvozX-xPk0wFeNxbZd_dM"
+
+## AD HOC: Because 'x' has both upper and lower-case symbols, which
+## shouldn't really be allowed. The R package rejects them. /HB 2023-10-21
+x <- toupper(x)
+
+stopifnot( 
+  lseguid_blunt(x) == truth,
+  lseguid_blunt(x) == useguid(paste0(c(rc(x), space(10L), reverse_sequence(x))))
+)
