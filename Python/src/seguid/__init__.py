ce29b1a2f9798da2ce65bcb6efad4ffcf49da06b
BjornFJohansson <bjornjobb@gmail.com>
2023-12-15 10:47:46 +0000

    only import the four main functions in __init__.py

--- Python/src/seguid/__init__.py
+++ Python/src/seguid/__init__.py
@@ -2,6 +2,3 @@ from seguid.chksum import slseguid
 from seguid.chksum import scseguid
 from seguid.chksum import dlseguid
 from seguid.chksum import dcseguid
-
-from seguid.chksum import tuple_from_repr
-from seguid.chksum import repr_from_tuple
