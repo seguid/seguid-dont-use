ead1bb09334df8004a06a1db0a93f94d76e33abb
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-12-17 19:33:07 -0800

    R: Add min_rotation()

--- r/R/manip.R
+++ r/R/manip.R
@@ -45,5 +45,56 @@ rc <- function(seq, table = COMPLEMENT_TABLE_DNA) {
 }
 
 
-min_rotation_py <- function(s, table = COMPLEMENT_TABLE_DNA) {
+min_rotation <- function(s, table = COMPLEMENT_TABLE_DNA) {
+  assert_table(table)
+  assert_in_alphabet(s, alphabet = names(table))
+
+  ## Turn string into character vector
+  s <- strsplit(s, split = "", fixed = TRUE)[[1]]
+
+  prev <- NULL
+  rep <- 0
+
+  ds <- c(s, s)
+  lens <- length(s)
+  lends <- lens * 2
+  old <- 1
+  k <- 1
+  w <- character(0)
+  while (k <= lends) {
+    i <- k
+    j <- k + 1
+#    str(list(k = k, i = i, j = j, ds_i = ds[i], ds_j = ds[j], old = old, prev = prev, w = w))
+    
+    while (j <= lends && ds[i] <= ds[j]) {
+      if (ds[i] == ds[j]) {
+        i <- i + 1
+      } else {
+        i <- k
+      }
+      j <- j + 1
+    }
+#    str(list(i = i, j = j, ds_i = ds[i], ds_j = ds[j]))
+    
+    while (k < i + 1) {
+      k <- k + (j - i)
+      prev <- w
+      w <- ds[old:(k-1)]
+#      str(list(k = k, i = i, j = j, old = old, prev = prev, w = w))
+      
+      old <- k
+      if (identical(w, prev)) {
+        rep <- rep + 1
+      } else {
+        prev <- w
+        rep <- 1
+      }
+      if (length(w) * rep == lens) {
+#        message("Found: ", old - i)
+        return(old - i)
+      }
+    }
+  } ## while (k < lends)
+  
+  0L
 }
