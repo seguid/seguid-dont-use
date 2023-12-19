d857948cffa91d959d3a9abd1ec8ce9af371d710
BHGC Website GHA Workflow Runner <bhgc-github-actions-publish@bhgc.org>
2023-12-18 22:32:30 -0800

    R: Add support for --file=<file> argument and no-named argument + --debug [#25]

--- r/R/zzz.cli.R
+++ r/R/zzz.cli.R
@@ -17,21 +17,62 @@ Copyright: Henrik Bengtsson (2023)
 License: MIT
 '
 
-cli_call_fcn <- function(fcn, ...) {
+#' @importFrom utils file_test
+cli_call_fcn <- function(..., file = NULL, debug = FALSE, fcn) {
   if (is.character(fcn)) {
     fcn <- get(fcn, mode = "function", envir = getNamespace(.packageName), inherits = FALSE)
   }
+  stopifnot(length(debug) == 1, is.logical(debug), !is.na(debug))
+  
+  seq <- NULL
+  
+  args <- list(...)
+  nargs <- length(args)
+  if (nargs > 0) {
+    names <- names(args)
+    if (is.null(names)) names <- rep("", times = nargs) 
+    ## At most one unnamed CLI option
+    unnamed <- which(nchar(names) == 0)
+    if (length(unnamed) > 1) {
+      unknown <- unnamed[-length(unnamed)]
+      stop("Unknown option: ", paste(sQuote(unknown), collapse = ", "))
+    }
+    seq <- args[[unnamed]]
+    if (!is.null(file)) {
+      stop("Option --file=<filename> already specified")
+    }
+  }
 
-  ## Get arguments
-  seq <- readLines('stdin')
+  if (is.null(seq)) {
+    ## Get sequence input from file?
+    if (!is.null(file)) {
+      if (!file_test("-f", file)) {
+        stop("No such file: ", sQuote(file))
+      }
+      seq <- readLines(file)
+    } else {
+      ## Get arguments from the standard input
+      seq <- readLines("stdin")
+    }
+  }
+  seq <- paste(seq, collapse = "\n")
 
+  if (debug) {
+    message(sprintf("Sequence data:\n%s", seq))
+  }
+  
   argnames <- names(formals(fcn))
   if (is.element("crick", argnames)) {
-    seq <- paste(seq, collapse = "\n")
     args <- tuple_from_repr(seq)
+    if (debug) {
+      msg <- sprintf("Sequence tuple:\nwatson=%s\ncrick=%s", args[[1]], args[[2]])
+    }
     if (!is.element("overhang", argnames)) {
       args <- args[-3]
+    } else if (debug) {
+      msg <- sprintf("%s\noverhang=%d", msg, args[[3]])
     }
+    if (debug) message(msg)
   } else {
     args <- list(seq)
   }
@@ -44,29 +85,29 @@ cli_call_fcn <- function(fcn, ...) {
 
 
 class(seguid) <- c("cli_function", class(seguid))
-attr(seguid, "cli") <- function(type = c("seguid", "slseguid", "scseguid", "dlseguid", "dcseguid"), ...) {
+attr(seguid, "cli") <- function(..., type = c("seguid", "slseguid", "scseguid", "dlseguid", "dcseguid")) {
   type <- match.arg(type)
-  cli_call_fcn(type, ...)
+  cli_call_fcn(..., fcn = type)
 }
 
 class(slseguid) <- c("cli_function", class(slseguid))
 attr(slseguid, "cli") <- function(...) {
-  cli_call_fcn(slseguid, ...)
+  cli_call_fcn(..., fcn = slseguid)
 }
 
 class(scseguid) <- c("cli_function", class(scseguid))
 attr(scseguid, "cli") <- function(...) {
-  cli_call_fcn(scseguid, ...)
+  cli_call_fcn(..., fcn = scseguid)
 }
 
 class(dlseguid) <- c("cli_function", class(dlseguid))
 attr(dlseguid, "cli") <- function(...) {
-  cli_call_fcn(dlseguid, ...)
+  cli_call_fcn(..., fcn = dlseguid)
 }
 
 class(dcseguid) <- c("cli_function", class(dcseguid))
 attr(dcseguid, "cli") <- function(...) {
-  cli_call_fcn(dcseguid, ...)
+  cli_call_fcn(..., fcn = dcseguid)
 }
 
 
